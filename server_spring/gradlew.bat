86f02e5cc5927ab97e9284d55b1368404a188148
kseon0828 <kseon0828@gmail.com>
2022-08-19 22:41:08 +0900

    todo 조회 수정 -> work

--- server_spring/gradlew.bat
+++ server_spring/gradlew.bat
@@ -14,7 +14,7 @@
 @rem limitations under the License.
 @rem
 
-@if "%DEBUG%" == "" @echo on
+@if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
 @rem  Gradle startup script for Windows
